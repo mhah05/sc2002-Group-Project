41393b8d50518d92678ff6e70017170a00bd5aad
Qvs5234 <sjp523481@gmail.com>
2026-04-16 00:17:22 +0800

    Update ItemAction.java

--- (Suo) Action & status/Action/ItemAction.java
+++ (Suo) Action & status/Action/ItemAction.java
@@ -3,26 +3,33 @@ import java.util.List;
 import combatants.Combatant;
 import ui.GameUI;
 import items.Item;
+import combatants.Player;
 public class ItemAction extends AbstractAction {
+    private Item selectedItem;
     public ItemAction() {
         super(item.getName());
     }
+    public List<Combatant> getTargets(Combatant user, List<Combatant> target, GameUI ui){
+        List<Combatant> targets = new ArrayList<>();
+        Player player = (Player) user;
+        List<Item> items = player.getItems();
+        if (inventory.isEmpty()) {
+            System.out.println("No available items!");
+            return targets;
+        }
+        this.selectedItem = ui.getPlayerItem(items);
+        targets.add(user);
+        return targets;
+    }
     public void execute(Combatant user, List<Combatant> targets) {
         //check available
+        if (selectedItem == null) return;
         if (item.isConsumed()) {
             System.out.println(item.getName() + " is already used up!");
             return;
         }
         //decide item to use
-        ui.getPlayerItem();
         System.out.println(user.getName() + " → Item");
-        item.use(user);
-        
+        selectedItem.use(user);
     }
-    public List<Combatant> getTargets(Combatant user, List<Combatant> target, GameUI ui){
-        List<Combatant> targets = new ArrayList<>();
-        targets.add(user);
-        return targets;
-    }
-
 }
