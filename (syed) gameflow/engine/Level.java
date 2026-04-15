6f4588814738b99cf05c96c051ab86eb975666c7
msyed0310 <msyed0310@gmail.com>
2026-04-15 17:57:24 +0800

    added Level.java

---
+++ (syed) gameflow/engine/Level.java
@@ -0,0 +1,39 @@
+package engine;
+
+import Combatants.Enemy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level 
+{
+	private int levelNumber;
+    private String difficulty;
+    private List<Enemy> initialEnemies;
+    private List<Enemy> backupEnemies;
+    
+    // Constructor
+    public Level(int levelNumber, String difficulty) 
+    {
+        this.levelNumber = levelNumber;
+        this.difficulty = difficulty;
+        this.initialEnemies = new ArrayList<>();
+        this.backupEnemies = new ArrayList<>();
+    }
+
+    // Getters
+    public int getLevelNumber()            { return levelNumber; }
+    public String getDifficulty()          { return difficulty; }
+    public List<Enemy> getInitialEnemies() { return initialEnemies; }
+    public List<Enemy> getBackupEnemies()  { return backupEnemies; }
+    public boolean hasBackupSpawn()        { return !backupEnemies.isEmpty(); }
+    
+    //Setters
+    public void setInitialEnemies(List<Enemy> enemies) 
+    { 
+        this.initialEnemies = enemies; 
+    }
+    public void setBackupEnemies(List<Enemy> enemies)  
+    { 
+        this.backupEnemies = enemies; 
+    }
+}
