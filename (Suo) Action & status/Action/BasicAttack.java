acef0b579276795448f10e6bb0078ce01a576a9c
Qvs5234 <sjp523481@gmail.com>
2026-04-15 20:46:42 +0800

    Update BasicAttack.java

--- (Suo) Action & status/Action/BasicAttack.java
+++ (Suo) Action & status/Action/BasicAttack.java
@@ -9,4 +9,27 @@ public class BasicAttack extends AbstractAction{
         System.out.println(user.getName() + " -> BasicAttack -> " + target.getName() + ":");
         target.takeDamage(user.getAttack());
     }
+    public List<Combatant> getTargets(Combatant user, List<Combatant> target, GameUI ui){
+        List<Combatant> targets = new ArrayList<>();
+        if (user instanceof Enemy) {
+            for (Combatant c : target) {
+                if (c instanceof Player && c.isAlive()) {
+                    targets.add(c);
+                    break;
+                }
+            }
+        }else {
+            List<Combatant> aliveEnemies = new ArrayList<>();
+            for (Combatant c : target) {
+                if (c instanceof Enemy && c.isAlive()) {
+                    aliveEnemies.add(c);
+                }
+            }
+            if (!aliveEnemies.isEmpty()) {
+                Combatant finaltarget = ui.getPlayerTarget(aliveEnemies);
+                targets.add(finaltarget);
+            }
+        }
+        return targets;
+    }
 }
