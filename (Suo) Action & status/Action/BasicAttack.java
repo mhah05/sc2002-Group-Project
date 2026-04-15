b5dc098f4b6a265b77b9639e7781519619d2199e
Qvs5234 <sjp523481@gmail.com>
2026-04-15 15:56:30 +0800

    Update BasicAttack.java

--- (Suo) Action & status/Action/BasicAttack.java
+++ (Suo) Action & status/Action/BasicAttack.java
@@ -6,13 +6,7 @@ public class BasicAttack extends AbstractAction{
         if (targets == null || targets.isEmpty())
             return;
         Combatant target = targets.get(0);
-        //we need to define in game engine a new list, the target included
-
-        //To be confirmed: should we do calculation here? or do in takeDamage
-        //int rawDamage = user.getAttack()-target.getDefense();
-        //int finalDamage = Math.max(0,rawDamage);
         System.out.println(user.getName() + " -> BasicAttack -> " + target.getName() + ":");
-        //target.takeDamage(finalDamage);
         target.takeDamage(user.getAttack());
     }
 }
