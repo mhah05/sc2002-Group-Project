45cc65e1a3201361015fd42fa0bece9819d1c876
TrizzyTurtle <tristan@mota.asia>
2026-04-15 23:11:11 +0800

    updated PowerStone -> GameEnginge will treat as a SpecialSkill with not cooldown

--- (Tristan) Combatants & Items Class Diagrams/Items/PowerStone.java
+++ (Tristan) Combatants & Items Class Diagrams/Items/PowerStone.java
@@ -10,9 +10,6 @@ public class PowerStone extends AbstractItem{
         super("Power Stone");
     }
     public void use(Player user){
-        Action skill = user.getSpecialSkill();
-        List<Combatant> targets = skill.getTargets()
-        skill.execute(user,BasicAttack.getTargets(user));
         this.markConsumed();
     }
 }
