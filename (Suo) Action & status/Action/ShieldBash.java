ea3413d2523bf827a7660f9a8b152237664ff0c5
Qvs5234 <sjp523481@gmail.com>
2026-04-15 21:01:45 +0800

    Update ShieldBash.java

--- (Suo) Action & status/Action/ShieldBash.java
+++ (Suo) Action & status/Action/ShieldBash.java
@@ -8,4 +8,21 @@ public class ShieldBash extends SpecialSkill {
         target.takeDamage(user.getAttack());
         target.addStatusEffect(new StunEffect());
     }
+    
+    public List<Combatant> getTargets(Combatant user, List<Combatant> target, GameUI ui){
+        List<Combatant> targets = new ArrayList<>();
+        if (user instanceof Player) {
+            List<Combatant> aliveEnemies = new ArrayList<>();
+            for (Combatant c : target) {
+                if (c instanceof Enemy && c.isAlive()) {
+                    aliveEnemies.add(c);
+                }
+            }
+            if (!aliveEnemies.isEmpty()) {
+                Combatant finaltarget = ui.getPlayerTarget(aliveEnemies);
+                targets.add(finaltarget);
+            }
+        }
+        return targets;
+    }
 }
