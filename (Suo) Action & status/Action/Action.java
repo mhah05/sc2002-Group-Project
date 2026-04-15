2875e5c5e2e9f2c8f2332727f9cb2afec7b81c67
Qvs5234 <sjp523481@gmail.com>
2026-04-15 20:47:18 +0800

    Enhance Action interface with getTargets method
    Added getTargets method to Action interface.

--- (Suo) Action & status/Action/Action.java
+++ (Suo) Action & status/Action/Action.java
@@ -1,4 +1,6 @@
 public interface Action {
     void execute(Combatant user, List<Combatant> targets);
     public String getName();
+    List<Combatant> getTargets(Combatant user, List<Enemy> enemies, GameUI ui);
+
 }
