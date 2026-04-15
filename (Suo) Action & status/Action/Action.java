3b9987c23852e98dd624a58927be1bfa62426195
Qvs5234 <sjp523481@gmail.com>
2026-04-15 21:22:18 +0800

    Update Action.java

--- (Suo) Action & status/Action/Action.java
+++ (Suo) Action & status/Action/Action.java
@@ -1,6 +1,6 @@
 public interface Action {
     void execute(Combatant user, List<Combatant> targets);
     public String getName();
-    List<Combatant> getTargets(Combatant user, List<Enemy> enemies, GameUI ui);
+    List<Combatant> getTargets(Combatant user, List<Combatant> enemies, GameUI ui);
 
 }
