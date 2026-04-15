76b24d87dc9b075043304c14e84a27604caaef14
Qvs5234 <sjp523481@gmail.com>
2026-04-15 20:48:40 +0800

    Update Defend.java

--- (Suo) Action & status/Action/Defend.java
+++ (Suo) Action & status/Action/Defend.java
@@ -5,4 +5,10 @@ public class Defend extends AbstractAction {
     public void execute(Combatant user, List<Combatant> targets) {
         user.addStatusEffect(new DefendEffect());
     }
+    public List<Combatant> getTargets(Combatant user, List<Combatant> target, GameUI ui) {
+        // targets always the user
+        List<Combatant> targets = new ArrayList<>();
+        targets.add(user);
+        return targets;
+    }
 }
