91b017e9d4368ee7141c526c48a2ee07d20bf615
Qvs5234 <sjp523481@gmail.com>
2026-04-15 21:49:35 +0800

    Enhance SpecialSkill with target selection method
    Added abstract method getTargets to SpecialSkill class.

--- (Suo) Action & status/Action/SpecialSkill.java
+++ (Suo) Action & status/Action/SpecialSkill.java
@@ -1,6 +1,11 @@
 package actions;
+import java.util.List;
+import combatants.Combatant;
+import ui.GameUI;
 public abstract class SpecialSkill extends AbstractAction {
     public SpecialSkill(String name) {
         super(name);
     }
+    public abstract List<Combatant> getTargets(Combatant user, List<Combatant> enemies, GameUI ui);
+
 }
