f3a129eeae1495b23b3e34499e2dd7b2787224f3
Qvs5234 <sjp523481@gmail.com>
2026-04-16 18:50:04 +0800

    Update SmokeBombEffect.java

--- (Suo) Action & status/StatusEffect/SmokeBombEffect.java
+++ (Suo) Action & status/StatusEffect/SmokeBombEffect.java
@@ -5,7 +5,7 @@ public class SmokeBombEffect extends AbstractStatusEffect {
         super("SmokeBombEffect", 1);
     }
     public void apply(Combatant target){
-        System.out.println(" → Smoke Bomb used: Enemy attacks deal 0 damage this turn + next");
+        System.out.println(" Enemy attacks deal 0 damage this turn + next");
         //this case the target will be user itself
     }
 }
